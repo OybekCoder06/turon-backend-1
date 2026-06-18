4429f9adc96d8689a50f28172860ecb8efd5e503
Oybek KHAKIMOV <xokimovoybek@gmail.com>
2026-06-18 09:59:50 +0500

    venv-pip

---
+++ 8-dars/main.py
@@ -0,0 +1,17 @@
+# import camelcase
+
+# c = camelcase.CamelCase()
+
+# txt = """Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Maecenas porttitor congue massa. Fusce posuere, magna sed pulvinar ultricies, purus lectus malesuada libero, sit amet commodo magna eros quis urna.
+# Nunc viverra imperdiet enim. Fusce est. Vivamus a tellus.
+# Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Proin pharetra nonummy pede. Mauris et orci.
+# Aenean nec lorem. In porttitor. Donec laoreet nonummy augue.
+
+# """
+
+# print(c.hump(txt))
+
+
+
+import cowsay 
+cowsay.cow("Hello World!")
